95bbc65f020b36e0a85d2de0381495fa0eb19089
RuiMeng1 <rmen0016@student.monash.edu>
2024-08-01 14:52:05 +1000

    cubes for w2

--- workshop2/src/flip.ts
+++ workshop2/src/flip.ts
@@ -1,8 +1,20 @@
-import { show,IMPLEMENT_THIS, IMPLEMENT_THIS_TYPE } from './show'
+import { show,IMPLEMENT_THIS, IMPLEMENT_THIS_TYPE } from './show' // WTF are we importing????
 
 type BinaryFunction<T,U,V> = (x:T, y:U) => V
 type CurriedFunction<T,U,V> = (x:T) => (y:U) => V
 
+/*
+            Explanation:
+
+            power = x => y => Math.pow(x,y);
+            power(2) = y => Math.pow(2,y);
+
+            Therefore
+            a.map(power(2)) --> a.map(y=>Math.pow(2,y))
+            where y is the items in array
+
+            [1,2,3,4,5,6,7,8].map(power(2)) = [2, 4, 8, 16, 32, 64, 128, 256 ]
+*/
 const curry : <T,U,V>(f:BinaryFunction<T,U,V>) => CurriedFunction<T,U,V> 
             = f=>x=>y=>f(x,y)
 const power = curry(Math.pow)
@@ -10,15 +22,23 @@ const a = [1,2,3,4,5,6,7,8]
 const twoToTheN = a.map(power(2))
 
 show(twoToTheN)('twoToTheN')
-// [2, 4, 8, 16, 32, 64, 128, 256 ]
 
 // ======= EXERCISE 1 =============================
 // implement flip and then use it with power to compute cubes, below
-const flip : <T,U,V>(f:CurriedFunction<T,U,V>) => CurriedFunction<U,T,V>
-           = IMPLEMENT_THIS
 
-const cubes: number[] = IMPLEMENT_THIS /* do something with flip and power and the number 3 here */
-show(cubes)('cubes')
-// [1, 8, 27, 64, 125, 216, 343, 512]
+/*
+        Reminder: power = x => y => Math.pow(x,y);
+
+        flip takes a function : f = x => y => V
+        and returns it with the arguments flipped : f = y => x => V
+*/
+
+const flip : <T,U,V>(f:CurriedFunction<T,U,V>) => CurriedFunction<U,T,V>
+            = f => x => y => f(y)(x);
+
+const flippedPower = flip(power); // flippedPower = y => x => Math.pow(y,x);
+
+const cubes: number[] = a.map(flippedPower(3)) // [1, 8, 27, 64, 125, 216, 343, 512]
+show(cubes)('cubes') 
 
 export { curry, flip, twoToTheN, cubes }
